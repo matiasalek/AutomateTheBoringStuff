b63936e3caf16b785adab50f6cd501b6e370f67d
Matias Aleksandrowicz <31048494+matiasalek@users.noreply.github.com>
2020-07-25 02:41:59 -0300

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,14 @@
+# List are arrays in other programming languages.
+
+list = ['cat', 'bat', 'rat', 'elephant']
+
+# list are indexed, starting at 0. Cat is position 0, bat is position 1 and so on.
+
+list[2] #rat
+
+#can start index in negative like -1 which is the last element from end to start position.
+# for example : list[-1] is elephant.
+
+list[1:3] #goes from position 1, bat, to rat, because 3 is not included
+#this is called slice
+#can use slices to replace elements inside a list
