863be5823457c6cefa624b951f6d2491ca4a4616
Matias Aleksandrowicz <31048494+matiasalek@users.noreply.github.com>
2020-07-24 22:48:20 -0300

    Create FirstProgram.py

---
+++ FirstProgram.py
@@ -0,0 +1,15 @@
+# This program says hello and ask for my name
+
+print ('Hello World!')
+
+
+print ('What is your name?') #ask for my name
+myName = input()
+print ('It is good to meet you, ' + myName)
+print ('The lenght of your name is: ')
+print (len(myName))
+
+
+print ('What is your age?') #ask for my age
+myAge = input()
+print ('You will be ' + str(int(myAge) + 10) + ' in tean years.')
