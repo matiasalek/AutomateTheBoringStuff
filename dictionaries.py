88f84dd567882aff03286afa654acfe1f38e469d
Matias Aleksandrowicz <31048494+matiasalek@users.noreply.github.com>
2020-07-29 03:07:07 -0300

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,12 @@
+# dictionaries are like list (arrays) but can store multiple data types (like structs in c++)
+
+#dictionary data type
+myCat = {
+    'size': 'fat',
+    'color': 'grey',
+    'disposition': 'fat'
+}
+
+#learned about methods and pprint
+
+#basic data structures
